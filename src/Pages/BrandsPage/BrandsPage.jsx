8179950cf2f1c3d5473a64ca9daee80e8a2117cd
Maha <kaverron1@gmail.com>
2022-11-29 15:25:47 +0300

    react-Project

--- src/Pages/BrandsPage/BrandsPage.jsx
+++ src/Pages/BrandsPage/BrandsPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from '../../Layout/Header/Header'
 import Vintage from './Vintage/Vintage'
 import Deha from './Deha/Deha'
 import DehaAbout from './DehaAbout/DehaAbout'
